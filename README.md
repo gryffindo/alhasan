eb7f068c08e66c069237d5d2406f09ec6582d09e
Sakib Al Hasan <76560286+saifsaki@users.noreply.github.com>
2021-05-29 02:34:23 +0600

    These are the source code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# sakib-new
-This is a new version of my personal professional website 
+## Sakib Al Hasan's new portfolio website.
+
+If you want to run this in local machine, all you need to do is just to download the dependencies by `$$ npm install`
+
+After Installations, we just have to simply run `$$ npm start`. That's All.
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(express.static('public'))
+app.use('/scripts', express.static(__dirname + 'public/'))
+app.use('/img', express.static(__dirname + 'public/styles'))
+app.use('/styles', express.static(__dirname + 'public/styles'))
+
+
+
+app.set('view engine', 'ejs')
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use('/css', express.static(__dirname + ('views/styles/')))
+
+
+app.use('/awards', (req, res) => {
+	res.render('pages/awards')
+})
+app.use('/portfolio', (req, res) => {
+	res.render('pages/portfolio')
+})
+app.use('/article', (req, res) => {
+	res.render('pages/article')
+})
+app.use('/', (req, res) => {
+	res.render('pages/main')
+})
+
+const PORT = process.env.PORT || 3031
+
+app.listen(PORT, () => {
+	console.log(`Server is running at ${PORT}`)
+})
