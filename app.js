86e06c40f1165addba22b5b612e89b0ed4ff90a6
Sakib Al Hasan <76560286+saifsaki@users.noreply.github.com>
2021-06-02 02:38:18 +0600

    Added Article Routes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const data = require('./data.json')
 const app = express();
 
 app.use(express.static("public"));
@@ -15,6 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/css", express.static(__dirname + "views/styles/"));
 
+
+// Article Pages Routes
+app.use("/article/1", (req, res) => {
+	let article1 = data.article1
+
+	res.render("pages/article/1", {
+		article1
+	})
+})
+app.use("/article/2", (req, res) => {
+	res.render("pages/article/2")
+})
+// Routes
 app.use("/awards", (req, res) => {
   res.render("pages/awards");
 });
@@ -22,11 +35,26 @@ app.use("/portfolio", (req, res) => {
   res.render("pages/portfolio");
 });
 app.use("/article", (req, res) => {
-  res.render("pages/article");
+
+		// article data
+		let article1 = data.article1
+		let article2 = data.article2
+  res.render("pages/article", {
+		article1,
+		article2
+	});
 });
+
+
+
 app.use("/", (req, res) => {
+
+
   res.render("pages/main");
 });
+
+
+
 
 const PORT = process.env.PORT || 3031;
 
