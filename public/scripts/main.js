e26542d07eac12c1e526a20166b2cbdf441c42ac
Sakib Al Hasan <76560286+saifsaki@users.noreply.github.com>
2021-06-17 21:14:13 +0600

    edited scrollbar

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,44 @@
 const toggle = document.getElementById("toggle");
-    const sidebar = document.getElementById("sidebar");
+const sidebar = document.getElementById("sidebar");
 
-    document.onclick = function (e) {
-      if (e.target.id !== "sidebar" && e.target.id !== "toggle") {
-        toggle.classList.remove("active");
-        sidebar.classList.remove("active");
-      }
-    };
+document.onclick = function (e) {
+  if (e.target.id !== "sidebar" && e.target.id !== "toggle") {
+    toggle.classList.remove("active");
+    sidebar.classList.remove("active");
+  }
+};
 
-    toggle.onclick = function () {
-      toggle.classList.toggle("active");
-      sidebar.classList.toggle("active");
-    };
+toggle.onclick = function () {
+  toggle.classList.toggle("active");
+  sidebar.classList.toggle("active");
+};
+
+
+function showTime() {
+	var date = new Date();
+	var h = date.getHours(); // 0 - 23
+	var m = date.getMinutes(); // 0 - 59
+	var s = date.getSeconds(); // 0 - 59
+	var session = "AM";
+
+	if (h == 0) {
+		h = 12;
+	}
+
+	if (h > 12) {
+		h = h - 12;
+		session = "PM";
+	}
+
+	h = h < 10 ? "0" + h : h;
+	m = m < 10 ? "0" + m : m;
+	s = s < 10 ? "0" + s : s;
+
+	var time = h + ":" + m + ":" + s + " " + session;
+	document.getElementById("MyClockDisplay").innerText = time;
+	document.getElementById("MyClockDisplay").textContent = time;
+
+	setTimeout(showTime, 1000);
+}
+
+showTime();
